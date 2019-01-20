2aedd3a57b6a65a77b551bb1e2a7986b36435af5
samirmishra2004 <samirmishra2004@users.noreply.github.com>
2019-01-20 20:23:21 +0000

    Header and Footer
    Added header and footer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Button } from "reactstrap";
+import Header from "./components/Header.js";
+import Footer1 from "./components/Footer1.js";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <Header />
+      <div id="content" />
+      <Footer1 />
     </div>
   );
 }
